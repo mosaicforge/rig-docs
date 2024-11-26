c402e908e0c803f6947855273f4dd8daf283419d
Mathieu <mathieu@playgrounds.network>
2024-11-25 23:14:42 -0500

    change config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ import nextra from 'nextra';
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  defaultShowCopyCode: true
+  defaultShowCopyCode: true,
+  staticImage: true
 });
 
 export default withNextra();
