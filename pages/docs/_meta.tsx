8b52b836de9c0e95f8b5516a2601d6b3373f3a68
Mathieu <mathieu@playgrounds.network>
2024-12-18 12:25:09 -0500

    fix: broken in-links

--- pages/docs/_meta.tsx
+++ pages/docs/_meta.tsx
@@ -1,4 +1,4 @@
-import { Blocks, BookText, CircleHelp, DraftingCompass, Landmark, Puzzle, Rocket, Unplug } from "lucide-react";
+import { Blocks, Lightbulb, CircleHelp, DraftingCompass, Landmark, Puzzle, Rocket, Unplug, MessageCircleQuestion } from "lucide-react";
 
 const iconStyle = {
   width: '1rem',
@@ -8,11 +8,12 @@ const iconStyle = {
 const meta = {
   "index": "Overview",
   "0_quickstart": {title: <><Rocket style={iconStyle} /> &nbsp; Quickstart</>},
-  "1_why_rig": {title: <><CircleHelp style={iconStyle} /> &nbsp; Why Rig</>},
-  "2_architecture": {title: <><Landmark style={iconStyle} /> &nbsp; Architecture</>},
-  "3_concepts": {title: <><Puzzle style={iconStyle} /> &nbsp; Concepts</>},
-  "4_integrations": {title: <><Unplug style={iconStyle} /> &nbsp; Integrations</>},
-  "5_extensions": {title: <><Blocks style={iconStyle} /> &nbsp; Extensions</>}
+  "1_why_rig": {title: <><Lightbulb style={iconStyle} /> &nbsp; Why Rig</>},
+  "2_faq": {title: <><MessageCircleQuestion style={iconStyle} /> &nbsp; FAQ</>},
+  "3_architecture": {title: <><Landmark style={iconStyle} /> &nbsp; Architecture</>},
+  "4_concepts": {title: <><Puzzle style={iconStyle} /> &nbsp; Concepts</>},
+  "5_integrations": {title: <><Unplug style={iconStyle} /> &nbsp; Integrations</>},
+  "6_extensions": {title: <><Blocks style={iconStyle} /> &nbsp; Extensions</>}
 }
 
 export default meta;
